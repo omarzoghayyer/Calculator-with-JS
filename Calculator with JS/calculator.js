86c9bd22182ca3212a81c71e80fcef95c84d42e0
Omar Zoghayyer <49251966+omarzoghayyer@users.noreply.github.com>
2020-01-07 19:49:55 -0800

    Update calculator.js

--- Calculator with JS/calculator.js
+++ Calculator with JS/calculator.js
@@ -1,4 +1,9 @@
-const newCal = () >= {
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
-};
-console.log(newCal);
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
